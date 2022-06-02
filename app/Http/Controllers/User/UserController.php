683779a976978318f435f350d4e87db4f474077a
reda <redabrim2002@gmail.com>
2022-06-02 22:35:58 +0100

    fix problem image registre

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -44,7 +44,7 @@ class UserController extends Controller
         }
         
         
-        $user->password = $request->password;
+        $user->password = Bcrypt($request->password);
         $save = $user->save();
 
         if( $save ){
